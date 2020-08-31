72feb4b7f7aea18a24848ded4892f2680e5c9a0c
zipizz <whfhxla@hanmail.net>
2020-09-01 01:14:54 +0900

    survive before 7.7 p.222

--- MyWebBrowser/app/src/main/java/com/survivecoding/mywebbrowser/MainActivity.kt
+++ MyWebBrowser/app/src/main/java/com/survivecoding/mywebbrowser/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.survivecoding.mywebbrowser
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +34,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         webView.loadUrl("http://www.google.com")
+
+        registerForContextMenu(webView)
     }
 
     override fun onBackPressed() {
@@ -34,5 +44,63 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.action_google, R.id.action_home -> {
+                webView.loadUrl("http://www.google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                webView.loadUrl("http://www.naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                webView.loadUrl("http://www.daum.net")
+                return true
+            }
+            R.id.action_call -> {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:031-123-4567")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent);
+                }
+                return true
+            }
+            R.id.action_send_text -> {
+                return true
+            }
+            R.id.action_email -> {
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.action_share -> {
+                return true
+            }
+            R.id.action_browser -> {
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
